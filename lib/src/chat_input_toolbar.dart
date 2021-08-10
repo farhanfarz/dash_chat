d34e7a042a3b94a88dc89ee70396d1efcabca493
Ayisha <ayishakp12@gmail.com>
2021-08-10 17:04:21 +0400

    check scroll issue

--- lib/src/chat_input_toolbar.dart
+++ lib/src/chat_input_toolbar.dart
@@ -199,11 +199,13 @@ class ChatInputToolbar extends StatelessWidget {
       // FocusScope.of(context).requestFocus(focusNode);
       SystemChannels.textInput.invokeMethod('TextInput.hide');
 
-      Timer(Duration(seconds: 1), () {
-        onSendTextfieild();
-      });
+      // Timer(Duration(seconds: 1), () {
+      //   onSendTextfieild();
+      // });
 
       await onSend(message);
+
+       onSendTextfieild();
 
       controller.text = "";
 
